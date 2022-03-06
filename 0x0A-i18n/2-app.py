afb2a8a16b749979b8b445b0c3301986809fe63a
oimoralest <oimoralest@gmail.com>
2022-03-06 12:21:48 -0500

    Third task

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+""" Simple message of welcoming
+Description:
+    to start working models i18n and l10n
+"""
+from flask_babel import Babel
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ config all objects to translate
+    attribute:
+        Languages allowed
+        default local
+        time zone
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ first index template to getting started translate
+    Args:
+        template rendering
+    """
+    return render_template('1-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ GET localization of my user
+    Returns:
+        the best languages apply to his/her zone
+    """
+    lang = Config.LANGUAGES
+    return request.accept_languages.best_match(lang)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', '5000')
