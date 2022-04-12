c5c99d50824b020568dcdf50795e70097072ba91
oimoralest <oimoralest@gmail.com>
2022-04-12 07:40:26 -0500

    Redis Basic
        Added advance tasks
    
    [project: 0x0B]

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Implementing an expiring web cache and tracker
+    obtain the HTML content of a particular URL and returns it """
+import redis
+import requests
+
+
+r = redis.Redis()
+count = 0
+
+
+def get_page(url: str) -> str:
+    """Tracks how many times a particular URL was accessed in the key
+    "count:{url}" and cache the result with an expiration time of 10 seconds
+
+    Arguments:
+        - url: URL
+
+    Return:
+        - URL content
+    """
+    r.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    r.incr(f"count:{url}")
+    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    return resp.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
